234c9e737504eb4900c562487a63ee2df03dc56b
Sondre Halvorsen <H153869@Sondres-MacBook-Pro.local>
2018-06-21 16:58:41 +0200

    First commit in cookiecutter test repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Trying out the cookiecutter project layout',
+    author='Sondre Halvorsen',
+    license='MIT',
+)
